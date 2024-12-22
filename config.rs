5f2ff7bb7aa37cd72351ec2229982dc99f04e2e5
W0nchichi <165472044+W0nchichi@users.noreply.github.com>
2024-12-22 13:19:56 +1000

    Updated table, added config file
    Writing to files seems so complex :(

---
+++ config.rs
@@ -0,0 +1,6 @@
+//I'm sure as the project gets bigger, it could do with a lot of reworking, but
+//For now, any hardcoded variables can go here for simplification
+//filePath for all Tables, maybe make it changeable later
+pub const DEFAULT_DIR: &str = "dbms_rust_project/data";
+//table name when none is supplied
+pub const DEFAULT_TABLE: &str = "default_table_name";
--- table/table.rs
+++ table/table.rs
@@ -1,3 +1,6 @@
+use std::path::Path;
+use crate::dbms_rust_project::config::DEFAULT_DIR;
+
 pub struct Table {
     pub name: String,
     pub field: HashMap<String, DataType>,
@@ -8,32 +11,77 @@ pub struct Table {
 impl Table{
     //I looked at SQLite Code and they store their files in binary file formats.
     //I am not entirely sure if I am comfortable with messing with that first so I think
-    //I'll try XML files or something first, lemme make an example of the format I'm thinking
-    /*
-    <employees>
-        <employee>
-            <id>1</id>
-            <name>John Doe</name>
-            <position>Manager</position>
-        </employee>
-        <employee>
-            <id>2</id>
-            <name>Jane Smith</name>
-            <position>Developer</position>
-        </employee>
-    </employees>
-     */
-    //It seems pretty annoying to do though, but i'll give it a shot soon, just very tired rn
-    pub fn Save(&mut self){
+    //After sleeping on it and checking Steel-db, they use something called a "columnar"
+    //file format which speeds up data retreival since you can just find the column that matches
+    //a condition right away. If 10GB of data is scanned in columnar, 300GB would have to be
+    //Scanned in CSV/XML
 
+    //I'm going to make a Columnar CSV file format, inspired by Matthew Rathbone's explanation
+    //if this was how you would store the data in CSV
+    /*
+    {
+    id: Integer,
+    first_name: String,
+    last_name: String,
+    age: Integer,
+    cool: Boolean,
+    favorite_fruit: Array[String]
+    }
+    Example:
+    1, Matthew, Rathbone, 19, True, ['bananas', 'apples']
+    2, Joe, Bloggs, 102, True,
+     */
+
+    //We'll store it like this in our CCSV
+    /*
+    Field Name/Field Type/Number of Characters:[data in csv format]
+    (except replace the Field Type with things from datatypes.rs[i32,f32,String])
+    For example:
+    ID/INT/3:1,2
+    FIRST_NAME/STRING/11:Matthew,Joe
+    LAST_NAME/STRING/15:Rathbone,Bloggs
+    AGE/INT/6:19,102
+    COOL/BOOL/3:1,1
+    FAVORITE_FRUIT/ARRAY[STRING]/19:[bananas,apples],[]
+     */
+    pub fn new() -> Table {
+        Table {
+            name: String::new(),
+            fields: HashMap::<String, DataType>::new(),
+            columns: HashMap::<String, Vec<DataType>>::new(),
+            select_columns: Vec::<String>::new(),
+        }
+    }
+
+    //for transparency, I couldn't figure out how to make a file, so this was taken from steel_db
+    pub fn instantiate_path_dir() {
+        if !Path::new(DEFAULT_DIR).exists(){
+            let result = std::fs::create_dir_all(DATA_DIR);
+            result.unwrap();
+        }
+    }
+
+    pub fn get_table_file_path(name: &String) -> String{
+        format!("{}/{}.ccsv", DATA_DIR, name)
+    }
+
+    pub fn Save(&mut self) -> Result<(), TableErrors> {
+        let in_memory_file_path = Table::get_table_file_path(&self.name);
+        let file_path = Path::new(&s);
     }
 
     pub fn Load(&mut self){
 
     }
     //I noticed on the steel-db page they don't make these public, honestly am too tired to see
-    pub fn get_table_name(&self) -> &String;
-    pub fn get_columns(&self) -> &HashMap<String, Vec<DataType>>;
-    pub fn get_select_columns(&self) -> &Vec<String>;
+    pub fn get_table_name(&self) -> &String {
+        return self.name;
+    }
+    pub fn get_columns(&self) -> &HashMap<String, Vec<DataType>>{
+        return self.columns;
+    }
+    pub fn get_select_columns(&self) -> &Vec<String>{
+        return self.select_columns;
+    }
 
 }
